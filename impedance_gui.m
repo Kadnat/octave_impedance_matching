fb5030d440d79db2b42864ff7bafbf068d9594fc
Kadnat <nathanael.blavoballarin@viacesi.fr>
2025-01-19 14:02:25 +0100

    update gui
    Signed-off-by: Kadnat <nathanael.blavoballarin@viacesi.fr>

--- impedance_gui.m
+++ impedance_gui.m
@@ -7,32 +7,41 @@
 function impedance_gui()
     fig = figure('Name','Impedance Matching GUI','NumberTitle','off','Position',[100 100 300 250]);
 
-    uicontrol(fig,'Style','text','String','Real(Z):','Position',[10 160 70 20]);
-    realField = uicontrol(fig,'Style','edit','Position',[80 160 100 20]);
+    uicontrol(fig,'Style','text','String','Load Real(Z):','Position',[10 160 70 20]);
+    loadrealField = uicontrol(fig,'Style','edit','Position',[80 160 100 20]);
 
-    uicontrol(fig,'Style','text','String','Imag(Z):','Position',[10 130 70 20]);
-    imagField = uicontrol(fig,'Style','edit','Position',[80 130 100 20]);
+    uicontrol(fig,'Style','text','String','Load Imag(Z):','Position',[10 130 70 20]);
+    loadimagField = uicontrol(fig,'Style','edit','Position',[80 130 100 20]);
 
-    uicontrol(fig,'Style','text','String','Z0 (Ω):','Position',[10 100 70 20]);
-    z0Field = uicontrol(fig,'Style','edit','Position',[80 100 100 20]);
+    uicontrol(fig,'Style','text','String','Source Real(Z):','Position',[10 100 70 20]);
+    sourcerealField = uicontrol(fig,'Style','edit','Position',[80 100 100 20]);
 
-    uicontrol(fig,'Style','text','String','Freq (prefixed):','Position',[10 40 90 20]);
-    freqField = uicontrol(fig,'Style','edit','Position',[100 40 100 20]);
+    uicontrol(fig,'Style','text','String','Source Imag(Z):','Position',[10 70 70 20]);
+    sourceimagField = uicontrol(fig,'Style','edit','Position',[80 70 100 20]);
+ 
+    uicontrol(fig,'Style','text','String','Z0 (Ω):','Position',[10 40 70 20]);
+    z0Field = uicontrol(fig,'Style','edit','Position',[80 40 100 20]);
+
+    uicontrol(fig,'Style','text','String','Freq (prefixed):','Position',[10 10 90 20]);
+    freqField = uicontrol(fig,'Style','edit','Position',[100 10 100 20]);
 
     % Boutton calcul
     uicontrol(fig,'Style','pushbutton','String','Calculer','Position',[190 140 80 30], ...
         'Callback', @(~,~) onCompute());
 
     function onCompute()
-        rVal = str2double(get(realField,'String'));
-        iVal = str2double(get(imagField,'String'));
+        loadrVal = str2double(get(loadrealField,'String'));
+        loadiVal = str2double(get(loadimagField,'String'));
+        sourcerVal = str2double(get(sourcerealField,'String'));
+        sourceiVal = str2double(get(sourceimagField,'String'));
+
         z0Val = parseSIprefix(get(z0Field,'String'));
         freqVal = parseSIprefix(get(freqField,'String'));
 
 
         close(fig);
-        zLoad = rVal + 1i*iVal;
-        zSource = 50;
+        zLoad = loadrVal + 1i*loadiVal;
+        zSource = sourcerVal + 1i*sourceiVal;
         impedance_matching(zSource, zLoad, z0Val, freqVal);
     end
 end
